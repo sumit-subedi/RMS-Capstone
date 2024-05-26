9f99db9b2a2e9322ad3eaf431c8910ed26898c37
Sumit Subedi <vanje.sumit@gmail.com>
2024-05-26 10:29:51 -0400

    Initial Commit

--- Server/index.js
+++ Server/index.js
@@ -1,33 +1,10 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql');
-
 
 // Import route handlers
 const waiterRoutes = require('./Routes/Waiterroutes');
 const receptionistRoutes = require('./Routes/Receptionroutes');
 const adminRoutes = require('./Routes/Adminroutes');
-
-// Create a connection pool
-const pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'restaurant_db'
-});
-
-// Test the connection
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.error('Error connecting to database: ', err);
-        return;
-    }
-    console.log('Connected to MySQL database!');
-    connection.release(); // Release the connection
-});
-
-module.exports = pool; 
 
 // Mount route handlers
 app.use('/waiter', waiterRoutes);
